8d736f68ab654246eca3b8d39cf2bb72d72e8cc8
Mike Repec <mikerepec@gmail.com>
2023-01-28 14:46:01 +0100

    Fix up redis initialization and caching

--- src/server/cache.ts
+++ src/server/cache.ts
@@ -1,11 +1,37 @@
 import type EventEmitter from 'events';
-import { createClient } from 'redis';
+import { createClient, SetOptions } from 'redis';
 
-const client = createClient({ url: import.meta.env.REDIS_URI });
+const DEFAULT_CACHE_EX = 30;
 
-(client as EventEmitter).on('error', (err: any) => console.log('Redis Client error', err));
-(client as EventEmitter).on('connect', () => console.log('Redis Client connected'));
+class Cache {
+  private client = createClient({ url: import.meta.env.REDIS_URI });
+  private isConnected = false;
 
-await client.connect();
+  constructor() {
+    (this.client as EventEmitter).on('connect', () => console.log('Redis Client connected'));
+  }
 
-export default client;
+  async get(key: string) {
+    await this.ensureConnection();
+    return this.client.get(key);
+  }
+
+  async set(key: string, value: string, opts: SetOptions) {
+    await this.ensureConnection();
+    await this.client.set(key, value, { EX: opts?.EX ?? DEFAULT_CACHE_EX });
+  }
+
+  private async ensureConnection() {
+    if (this.isConnected) return;
+
+    try {
+      await this.client.connect();
+      this.isConnected = true;
+    } catch (err) {
+      await this.client.disconnect();
+      throw err;
+    }
+  }
+}
+
+export default new Cache();
--- src/server/ip.ts
+++ src/server/ip.ts
@@ -4,19 +4,43 @@ import cache from './cache';
 
 const CACHE_TTL = 300; // 5 minutes
 
-export async function getFromCache(key: string): Promise<IpResponse | null> {
-  const raw = await cache.get(key);
-  if (!raw) return null;
+export async function getFromCache(ip: string): Promise<IpResponse | null> {
+  const key = cacheKey(ip);
+
+  const cached = await cache.get(key);
+  if (!cached) return null;
 
   console.log('Cache hit for:', key);
-  return { ...JSON.parse(raw), cached: true };
-}
-
-export async function saveToCache(key: string, value: IpResponse) {
-  await cache.set(key, JSON.stringify(value), { EX: CACHE_TTL });
+  return { ...JSON.parse(cached), cached: true };
 }
 
 export async function getFromAPI(ip: string): Promise<IpResponse> {
   console.log('Fetching IP Geolocation for:', ip || 'localhost');
-  return await fetch(`http://ip-api.com/json/${ip}`).then((res) => res.json());
+
+  const res = await fetch(`http://ip-api.com/json/${ip}`);
+  const ipData = await res.json();
+
+  if (ipData.status !== 'success') throw new Error(ipData.message);
+  return ipData;
 }
+
+export async function getFromAPIAndCache(ip: string): Promise<IpResponse> {
+  const ipResponse = await getFromAPI(ip);
+
+  await saveToCache(cacheKey(ip), ipResponse);
+  return ipResponse;
+}
+
+async function saveToCache(key: string, value: IpResponse) {
+  await cache.set(key, JSON.stringify(value), { EX: CACHE_TTL });
+}
+
+function cacheKey(ip: string): string {
+  return `tools.geo-ip.${ip || 'localhost'}`;
+}
+
+export default {
+  getFromAPI,
+  getFromAPIAndCache,
+  getFromCache,
+};
